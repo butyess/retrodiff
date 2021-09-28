4b55649f7afc63efd3ef4c4a33a9a7c50decad9a
Federico Masi <federico.masi.k333@gmail.com>
2021-09-28 13:02:59 +0200

    sources

---
+++ retrodiff/dag.py
@@ -0,0 +1,93 @@
+class Dag:
+    def __init__(self, input_nodes, output_node):
+        self.output_node = output_node
+        if set(input_nodes) != output_node.find_inputs():
+            raise ValueError("Invalid input nodes")
+        self.input_nodes = input_nodes
+
+    def forward(self, input_values):
+        assert len(input_values) == len(self.input_nodes)
+        for node, val in zip(self.input_nodes, input_values):
+            node.value = val
+        return self.output_node.value
+
+    def backward(self, output_grad):
+        order = self.topo_sort(self.output_node)
+        self.output_node.grad = output_grad
+        for node in order:
+            node.update_input_grads()
+        return [n.grad for n in self.input_nodes]
+
+    def topo_sort(self, node):
+        topo_order = []
+        queue = [node]
+        while len(queue) > 0:
+            n = queue.pop(0)
+            if n not in topo_order:
+                topo_order.append(n)
+                for i in n.input_nodes:
+                    queue.append(i)
+        return topo_order
+
+    def clear(self):
+        for node in self.topo_sort(self.output_node):
+            del node.value
+            node.grad = None
+
+
+class Node:
+    def __init__(self, function=None, input_nodes=[]):
+        self.function = function
+        self.input_nodes = input_nodes
+        self._value = None
+        self.grad = None
+
+    @property
+    def value(self):
+        if self._value is None:
+            ival = [i.value for i in self.input_nodes]
+            self._value = self.function.forward(*ival)
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        assert len(self.input_nodes) == 0, "not an input node"
+        self._value = value
+
+    @value.deleter
+    def value(self):
+        self._value = None
+
+    def find_inputs(self):
+        if len(self.input_nodes) == 0:
+            return set((self,))
+        return set.union(*[n.find_inputs() for n in self.input_nodes])
+
+    def update_input_grads(self):
+        ival = [i.value for i in self.input_nodes]
+        for i, n in enumerate(self.input_nodes):
+            if n.grad is None:
+                n.grad = self.function.backward(self.grad, i, *ival)
+            else:
+                # will `+=` always work? ∇ is linear, but...
+                n.grad += self.function.backward(self.grad, i, *ival)
+
+
+class Function:
+    def forward(self, *args):
+        '''
+        Computes the function.
+        '''
+        raise NotImplementedError
+
+    def backward(self, grad, wrt, *values):
+        '''
+        Performs backward pass with respect to the parameter `wrt`.
+        It returns dn/di * df/di, where df/di is the parameter `grad`,
+        and df/di depends on `values` and `wrt`.
+        '''
+        raise NotImplementedError
+
+    def __call__(self, *args):
+        return Node(function=self, input_nodes=list(args))
+
---
+++ retrodiff/nn.py
@@ -0,0 +1,68 @@
+import logging
+
+from retrodiff.dag import Dag, Node
+from retrodiff.fun import Function
+
+
+class NeuralNetwork:
+    '''
+    Base neural network class. To create your neural network override
+    `__init__` in a subclass and define the attributes `weights` and `_nn_dag`.
+    Then you can instantiate your network and `evaluate` or `train` it.
+    '''
+    def __init__(self):
+        self.weights = []
+        self._nn_dag = None
+        self._loss_dag = None
+        pass
+
+    def set_loss(self, loss_dag):
+        '''
+        Sets the loss function for the network.
+        `loss_dag` output should be a scalar.
+        `loss_dag` should have two input nodes, in this exact order:
+            1.  predicted values
+            2.  labels
+        '''
+        assert isinstance(loss_dag, Dag)
+        assert len(loss_dag.input_nodes) == 2
+        self._loss_dag = loss_dag
+
+    def train(self, lr, n_iterations, inputs, labels):
+        '''
+        Trains the network with gradient descent.
+        Set logging level to INFO to see progression.
+        '''
+        if self._loss_dag is None:
+            raise ValueError("Cannot train without a loss function")
+
+        for i in range(n_iterations):
+            logging.info("Iteration %d", i)
+            losses = 0
+            for x, y in zip(inputs, labels):
+                self._nn_dag.clear()
+                self._loss_dag.clear()
+
+                # forward
+                pred = self._nn_dag.forward([x] + self.weights)
+                loss = self._loss_dag.forward([pred, y])
+
+                # backward
+                loss_grad = self._loss_dag.backward(1) # loss should return a scalar, so 1 is ok
+                grads = self._nn_dag.backward(loss_grad[0])
+
+                for i, grad in enumerate(grads[1:]):
+                    self.weights[i] -= grad * lr
+
+                losses += loss
+            logging.info("Average loss: %d", losses / len(inputs))
+
+    def evaluate(self, inputs):
+        '''
+        Evaluate the network with the current weights.
+        '''
+        self._nn_dag.clear()
+        if not isinstance(inputs, list):
+            inputs = [inputs]
+        return self._nn_dag.forward(inputs + self.weights)
+
