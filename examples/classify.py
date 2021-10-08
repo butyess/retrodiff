b6993abc12fcf7136161c06ed9d4b900156ba990
Federico Masi <federico.masi.k333@gmail.com>
2021-10-08 10:21:56 +0200

    classifier example

--- examples/classify.py
+++ examples/classify.py
@@ -4,62 +4,100 @@ from functools import reduce
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
 
-from retrodiff import Dag, Function, Node
-from retrodiff.nn import NeuralNetwork
+from retrodiff import Dag, Node, Function
+from retrodiff.model import Model
+from retrodiff.utils import Dot, GradientDescent, ReLU, MSELoss
 
 
 logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 
-class Log(Function):
-    def forward(self, x): return math.log(x)
-    def backward(self, grad, wrt, x): return grad * (1/x)
-
-class Exp(Function):
-    def forward(self, x): return math.exp(x)
-    def backward(self, grad, wrt, x): return grad * math.exp(x)
-
-class Mul(Function):
-    def forward(self, x, y): return x * y
-    def backward(self, grad, wrt, x, y): return grad * (y, x)[wrt]
-
-class Add(Function):
-    def forward(self, *values): return sum(values)
-    def backward(self, grad, wrt, *values): return grad
-
-class Sub(Function):
-    def forward(self, x, y): return x - y
-    def backward(self, grad, wrt, x, y): return grad * (1, -1)[wrt]
-
-class Square(Function):
-    def forward(self, x): return x ** 2
-    def backward(self, grad, wrt, x): return grad * 2 * x
-
-class Dot(Function):
-    def forward(self, a, b): return np.dot(a, b)
-    def backward(self, grad, wrt, a, b):
-        if wrt == 0: return np.dot(grad, b.T)
-        else: return np.dot(a.T, grad)
+def test_loss():
+    loss_fn = SVMBinaryLoss()
+    preds = np.array([[1., 0.]])
+    label = np.array([1])
+    print(loss_fn.forward(preds, label))
+    print(loss_fn.backward(1, 0, preds, label))
+    print(loss_fn.backward(1, 1, preds, label))
 
 
-class Network(NeuralNetwork):
+def plot(model, inputs, labels):
+    h = 0.25
+    x_min, x_max = inputs[:, 0].min() - 1, inputs[:, 0].max() + 1
+    y_min, y_max = inputs[:, 1].min() - 1, inputs[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    input_mesh = np.c_[np.ravel(xx), np.ravel(yy)]
+    # xs = [x.reshape(1, -1) for x in input_mesh]
+    # scores = np.array([model.evaluate(x) for x in xs])
+    scores = model.evaluate(input_mesh)
+
+    Z = np.argmax(scores, axis=-1).reshape(xx.shape)
+
+    fig, ax = plt.subplots()
+    ax.contourf(xx, yy, Z, alpha=0.8, cmap=plt.cm.Spectral)
+    ax.scatter(inputs[:, 0], inputs[:, 1], c=labels, cmap=plt.cm.Spectral)
+    # ax.xlim(np.min(xx), np.max(xx))
+    # ax.ylim(yy.min(), yy.max())
+    plt.show()
+
+
+class SVMBinaryLoss(Function):
+    def __init__(self, margin=1):
+        self.margin = margin
+
+    def forward(self, preds, label):
+        # right = np.unravel_index(np.argmax(label), label.shape)
+        # wrong = np.unravel_index(np.argmin(label), label.shape)
+        return np.maximum(0, preds[:, int(not label)] - preds[:, label] + self.margin) / preds.shape[-1]
+
+    def backward(self, grad, wrt, preds, label):
+        if wrt == 0:
+            pd = np.ones(preds.shape) / 2
+            pd[:, label] *= -1
+            return pd
+        else:
+            return 0
+
+
+class Network(Model):
     def __init__(self, layers):
         super().__init__()
 
         p = [Node() for _ in layers]
-        dot = Dot()
+        pred, label = Node(), Node()
+        dot, relu = Dot(), ReLU()
+        loss = SVMBinaryLoss()
 
-        self._nn_dag = Dag(p, dot(reduce(lambda x, y: nonlinear(dot(x, y)), p[:-1]), p[-1]))
-        self.weights = [np.random.uniform(low=-4, high=4, size=dim) for dim in zip(layers[:-1], layers[1:])]
+        self._dag = Dag(p, dot(reduce(lambda x, y: relu(dot(x, y)), p[:-1]), p[-1]))
+        self._loss_dag = Dag([pred, label], loss(pred, label))
+        self._optim = GradientDescent(lr=0.00001)
+
+        self.parameters = [np.random.normal(scale=1.0, size=dim) for dim in zip(layers[:-1], layers[1:])]
 
 
 def main():
     model = Network([2, 16, 16, 16, 2])
 
-    pred, label = Node(), Node()
-    mul, add, sub, square = Mul(), Add(), Sub(), Square()
-    loss = Dag([pred, label], add(*[]))
+    inputs, labels = make_moons(n_samples=200, shuffle=True, noise=0.1)
+
+    x_train = [x.reshape(1, -1) for x in inputs]
+    y_train = [y.reshape(1, -1) for y in labels]
+
+    x_test = [x.reshape(1, -1) for x in inputs[75:]]
+    y_test = [y.reshape(1, -1) for y in labels[75:]]
+
+    model.train(20, x_train, y_train)
+    # print('avg loss: ', model.test(x_test, y_test))
+
+    pred = model.evaluate(x_test[0])
+    loss = model.loss(pred, y_test[0])
+    print('pred: ', pred, ' label: ', y_test[0], ' loss: ', loss)
+
+    plot(model, inputs, labels)
 
 
 if __name__ == "__main__":
