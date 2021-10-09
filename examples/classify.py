b97e865b4ef15e7e3d623760402ec3ca7a71a083
Federico Masi <federico.masi.k333@gmail.com>
2021-10-09 12:41:29 +0200

    cannot classify moons

--- examples/classify.py
+++ examples/classify.py
@@ -11,18 +11,6 @@ from retrodiff.model import Model
 from retrodiff.utils import Dot, GradientDescent, ReLU, MSELoss
 
 
-logging.basicConfig(format="%(message)s", level=logging.INFO)
-
-
-def test_loss():
-    loss_fn = SVMBinaryLoss()
-    preds = np.array([[1., 0.]])
-    label = np.array([1])
-    print(loss_fn.forward(preds, label))
-    print(loss_fn.backward(1, 0, preds, label))
-    print(loss_fn.backward(1, 1, preds, label))
-
-
 def plot(model, inputs, labels):
     h = 0.25
     x_min, x_max = inputs[:, 0].min() - 1, inputs[:, 0].max() + 1
@@ -31,8 +19,6 @@ def plot(model, inputs, labels):
                          np.arange(y_min, y_max, h))
 
     input_mesh = np.c_[np.ravel(xx), np.ravel(yy)]
-    # xs = [x.reshape(1, -1) for x in input_mesh]
-    # scores = np.array([model.evaluate(x) for x in xs])
     scores = model.evaluate(input_mesh)
 
     Z = np.argmax(scores, axis=-1).reshape(xx.shape)
@@ -40,8 +26,6 @@ def plot(model, inputs, labels):
     fig, ax = plt.subplots()
     ax.contourf(xx, yy, Z, alpha=0.8, cmap=plt.cm.Spectral)
     ax.scatter(inputs[:, 0], inputs[:, 1], c=labels, cmap=plt.cm.Spectral)
-    # ax.xlim(np.min(xx), np.max(xx))
-    # ax.ylim(yy.min(), yy.max())
     plt.show()
 
 
@@ -50,8 +34,6 @@ class SVMBinaryLoss(Function):
         self.margin = margin
 
     def forward(self, preds, label):
-        # right = np.unravel_index(np.argmax(label), label.shape)
-        # wrong = np.unravel_index(np.argmin(label), label.shape)
         return np.maximum(0, preds[:, int(not label)] - preds[:, label] + self.margin) / preds.shape[-1]
 
     def backward(self, grad, wrt, preds, label):
@@ -74,24 +56,31 @@ class Network(Model):
 
         self._dag = Dag(p, dot(reduce(lambda x, y: relu(dot(x, y)), p[:-1]), p[-1]))
         self._loss_dag = Dag([pred, label], loss(pred, label))
-        self._optim = GradientDescent(lr=0.00001)
+        self._optim = GradientDescent(lr=0.001)
 
-        self.parameters = [np.random.normal(scale=1.0, size=dim) for dim in zip(layers[:-1], layers[1:])]
+        self.parameters = [np.random.normal(scale=20.0, size=dim) for dim in zip(layers[:-1], layers[1:])]
 
 
 def main():
-    model = Network([2, 16, 16, 16, 2])
+    # logging.basicConfig(format="%(message)s", level=logging.INFO)
 
-    inputs, labels = make_moons(n_samples=200, shuffle=True, noise=0.1)
+    model = Network([2, 16, 16, 2])
 
-    x_train = [x.reshape(1, -1) for x in inputs]
-    y_train = [y.reshape(1, -1) for y in labels]
+    inputs, labels = make_moons(n_samples=100, shuffle=True, noise=0.1)
 
-    x_test = [x.reshape(1, -1) for x in inputs[75:]]
-    y_test = [y.reshape(1, -1) for y in labels[75:]]
+    cut = 75
+    batch_size = 20
 
-    model.train(20, x_train, y_train)
-    # print('avg loss: ', model.test(x_test, y_test))
+    x_train = [x.reshape(1, -1) for x in inputs[:cut]]
+    y_train = [y.reshape(1, -1) for y in labels[:cut]]
+
+    x_test = [x.reshape(1, -1) for x in inputs[cut:]]
+    y_test = [y.reshape(1, -1) for y in labels[cut:]]
+
+    for e in range(10):
+        ri = np.random.randint(0, len(x_train), size=batch_size)
+        model.train(10, [x_train[i] for i in ri], [y_train[i] for i in ri])
+        print('epoch ', e, 'avg loss: ', model.test(x_test, y_test))
 
     pred = model.evaluate(x_test[0])
     loss = model.loss(pred, y_test[0])
