fc2b1a9158ef82dbc9cbc8c80036114893c16b73
Federico Masi <federico.masi.k333@gmail.com>
2021-10-10 11:20:47 +0200

    momentum

--- retrodiff/utils/optim.py
+++ retrodiff/utils/optim.py
@@ -3,9 +3,16 @@ import numpy as np
 from ..model import Optimizer
 
 class GradientDescent(Optimizer):
-    def __init__(self, lr):
+    def __init__(self, lr, mu=0):
         self.lr = lr
+        self.mu = mu
+        self.vel = None
 
     def new_param(self, old_param, grads):
-        return [old - grad * self.lr for old, grad in zip(old_param, grads)]
+        if self.vel is None:
+            self.vel = [g for g in grads]
+        else:
+            self.vel = [self.mu * v + self.lr * g for v,g in zip(self.vel, grads)]
+
+        return [old - v for old, v in zip(old_param, self.vel)]
 
